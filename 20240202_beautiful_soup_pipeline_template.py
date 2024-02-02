a04c30d102f1ab74498f554f3dbb1c517f9ba1de
Yingquan Li <orbit196@gmail.com>
2024-02-02 03:33:41 -0500

    Update 20240202_beautiful_soup_pipeline_template.py

--- 20240202_beautiful_soup_pipeline_template.py
+++ 20240202_beautiful_soup_pipeline_template.py
@@ -15,7 +15,7 @@ def fetch_data(url):
     return response.text
 
 # Parsing the html using BeautifulSoup. Find all the <p> tags
-# and if the id attibutes exist, then save the id attribute and
+# and if the id attibute exists, then save the id attribute and
 # the corresponding value in a dictionary.
 def parse_data(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -32,13 +32,13 @@ def transform_data(data):
     """TBD: When you need to transform the data, modify this section."""
     return data
 
-# Data dump to store the data as json.
+# Do a data dump and store the data as json.
 def write_data(data, file_path):
     with open(file_path, 'w') as f:
         json.dump(data, f)
 
-# The data pipeline to run the sequence of steps in the data pipeline.
-# Run fetch_data(), then parse_data(), then transform_data(), and lastly
+# The data pipeline is organized in a sequence of steps to manipulate the 
+# data. Run fetch_data(), then parse_data(), then transform_data(), and lastly
 # write_data().
 def main(url, output_file_path):
     html = fetch_data(url)
@@ -46,5 +46,5 @@ def main(url, output_file_path):
     data = transform_data(data)
     write_data(data, output_file_path)
 
-# Run the pipeline
+# Run the pipeline.
 main('https://example.com', 'output.json')
