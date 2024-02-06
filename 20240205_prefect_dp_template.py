9c191e99fea502903cf6d95c63889ae7ce6be32d
Yingquan Li <orbit196@gmail.com>
2024-02-06 13:28:00 -0500

    Create 20240205_prefect_dp_template.py

---
+++ 20240205_prefect_dp_template.py
@@ -0,0 +1,42 @@
+"""
+Description: Data pipeline with Prefect template.
+
+Date: Sun. 2/5/24
+"""
+
+# Importing the required libraries.
+from prefect import Flow, Parameter, task
+from prefect.tasks.database import SQLiteScript
+from prefect.tasks.shell import ShellTask
+
+# Define the extract data task. Obviously when implementing your
+# own custom pipline, you have to change this.
+@task
+def extract_data(database_url):
+    data = SQLiteScript(database_url, script="SELECT * FROM your_table")
+    return data
+
+# Define the transform data task. Obviously when implementing your
+# own custom pipline, you have to change this.
+@task
+def transform_data(data):
+    transformed_data = data
+    return transformed_data
+
+# Define the load data task. Obviously when implementing your
+# own custom pipline, you have to change this.
+@task
+def load_data(data):
+    ShellTask(script=f"echo '{data}' > /path/to/your/output/file")
+
+# Defining a flow named "ETL". Notice that the flow takes in a parameter:
+# "database_url".
+with Flow("ETL") as flow:
+    database_url = Parameter("database_url")
+    data = extract_data(database_url)
+    transformed_data = transform_data(data)
+    load_data(transformed_data)
+
+# Running the flow with the Prefect engine. Notice how the parameter
+# is passed into the flow: {"parameter_name": "value"}.
+flow.run(parameters={"database_url": "sqlite:///your_database.db"})
