c9bf7a2fbe7691fb55e1d2fcd7f819e4d8eba8b1
Yingquan Li <orbit196@gmail.com>
2024-02-07 21:30:37 -0500

    Update 20240204_apache_airflow_dp_template.py

--- 20240204_apache_airflow_dp_template.py
+++ 20240204_apache_airflow_dp_template.py
@@ -9,12 +9,12 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
-# Define the extract function. Obviously when implementing your
+# Define the 'extract()' function. Obviously when implementing your
 # own custom pipeline, you have to change this.
 def extract(**kwargs):
     pass
 
-# Define the process function. Obviously when implementing your
+# Define the 'process()' function. Obviously when implementing your
 # own custom pipeline, you have to change this.
 def process(**kwargs):
     pass
