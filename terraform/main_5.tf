ec207fc4c42201b4b964f891cea479cff9208473
Yingquan Li <orbit196@gmail.com>
2024-04-01 17:29:47 -0400

    Create main_5.tf

---
+++ terraform/main_5.tf
@@ -0,0 +1,45 @@
+# Description: Terraform example 5.
+# Date: Tue. 2/20/24
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example_with_sg" {
+  ami           = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.example.id]
+
+  tags = {
+    Name = "example-instance-with-sg"
+  }
+}
